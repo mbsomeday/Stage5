9363f5d897adc5fc760ef813afd986a332f5f113
mbsomeday <137542994@qq.com>
2024-10-07 14:30:02 +0100

    update

--- dataloader.py
+++ dataloader.py
@@ -9,10 +9,10 @@ from PIL import Image
 
 
 ds_dict = {
-    'D1': r'D:\my_phd\dataset\Stage3\D1_ECPDaytime',
-    'D2': r'D:\my_phd\dataset\Stage3\D2_CityPersons',
-    'D3': r'D:\my_phd\dataset\Stage3\D3_ECPNight',
-    'D4': r'D:\my_phd\dataset\Stage3\D4_BDD100K',
+    'D1': r'/kaggle/input/stage4-d1-ecpdaytime-7augs',
+    'D2': r'/kaggle/input/stage4-d2-citypersons-7augs',
+    'D3': r'/kaggle/input/stage4-d3-ecpnight-7augs',
+    'D4': r'/kaggle/input/stage4-d4-7augs',
 }
 
 
